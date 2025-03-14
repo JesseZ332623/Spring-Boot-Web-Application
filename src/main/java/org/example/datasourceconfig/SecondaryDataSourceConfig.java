e9495f92ab23a66a8e1dcef7852978c783c07195
Jesse <3191955858@qq.com>
2025-03-15 00:10:44 +0800

    Country 数据源配置类

---
+++ src/main/java/org/example/datasourceconfig/SecondaryDataSourceConfig.java
@@ -0,0 +1,66 @@
+package org.example.datasourceconfig;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+@Configuration
+@EnableJpaRepositories(
+        basePackages            = "org.example.databaseaccess.countryinworld",
+        entityManagerFactoryRef = "secondaryEntityManagerFactory",
+        transactionManagerRef   = "secondaryTransactionManager"
+)
+public class SecondaryDataSourceConfig
+{
+    @Bean(name = "secondaryDataSource")
+    @ConfigurationProperties(prefix = "spring.datasource.secondary")
+    public DataSource secondaryDataSource()
+    {
+        return DataSourceBuilder.create()
+                                .type(HikariDataSource.class)
+                                .build();
+    }
+
+    @Bean(name = "secondaryEntityManagerFactory")
+    public LocalContainerEntityManagerFactoryBean secondaryEntityManagerFactory(
+            @Qualifier("secondaryDataSource") DataSource dataSource
+    )
+    {
+        var localContainerEntityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
+
+        localContainerEntityManagerFactoryBean.setDataSource(dataSource);
+        localContainerEntityManagerFactoryBean.setPackagesToScan("org.example.databaseaccess.countryinworld");
+        localContainerEntityManagerFactoryBean.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+
+        Map<String, Object> properties = new HashMap<>();
+        properties.put("hibernate.hbm2ddl.auto", "update");
+
+        localContainerEntityManagerFactoryBean.setJpaPropertyMap(properties);
+
+        return localContainerEntityManagerFactoryBean;
+    }
+
+    @Bean(name = "secondaryTransactionManager")
+    public PlatformTransactionManager secondaryTransactionManager(
+            @Qualifier("secondaryEntityManagerFactory")
+            LocalContainerEntityManagerFactoryBean entityManagerFactoryBean
+    )
+    {
+        return new JpaTransactionManager(
+                Objects.requireNonNull(entityManagerFactoryBean.getObject())
+        );
+    }
+}
