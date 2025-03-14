88d7ae2fd909e6be4b2569d27a6d42433abee2b3
Jesse <3191955858@qq.com>
2025-03-15 00:09:30 +0800

    Country 视图控制器类

---
+++ src/main/java/org/example/controller/countrycontroller/CountryViewController.java
@@ -0,0 +1,31 @@
+package org.example.controller.countrycontroller;
+
+import org.example.databaseaccess.countryinworld.Country;
+import org.example.service.countryqueryservice.CountryQueryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class CountryViewController
+{
+    private CountryQueryService countryQueryService;
+
+    @Autowired
+    public CountryViewController(CountryQueryService countryQueryService) {
+        this.countryQueryService = countryQueryService;
+    }
+
+    @GetMapping("/country/CountriesList")
+    public String listCountries(Model module)
+    {
+        List<Country> allCountries = this.countryQueryService.getAllCountries();
+
+        module.addAttribute("countries", allCountries);
+
+        return "CountriesList";
+    }
+}
