e01c38daa667120cbac5fedef00c0671407dc8f2
Jesse <3191955858@qq.com>
2025-03-15 23:20:56 +0800

    Spring Web 应用程序启动入口。

--- src/main/java/org/example/SpringBootServerStart.java
+++ src/main/java/org/example/SpringBootServerStart.java
@@ -4,9 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootServer
-{
+public class SpringBootServerStart {
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootServer.class, args);
+        SpringApplication.run(SpringBootServerStart.class, args);
     }
 }
